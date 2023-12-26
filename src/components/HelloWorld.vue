6d280f26e48919d71ff9243ac8a56c0019fbfed2
JrogeT <jorgerodrigotorrez@gmail.com>
2023-12-26 12:07:14 -0400

    Board created

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+import {computed, reactive} from 'vue';
+
+const properties = computed({
+  get: () => [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  set: val => {}
+})
+</script>
+
+<template>
+  <div class="bg-success-subtle d-flex flex-wrap">
+    <div class="bg-warning-subtle d-flex mx-auto">
+      <div class="border border-dark" style="width: 8rem; height: 8rem;">
+        start
+      </div>
+      <div v-for="property in properties" :key="property" class="border border-dark" style="width: 7rem; height: 8rem;">
+        Cell#{{property}}
+      </div>
+      <div class="border border-dark" style="width: 8rem; height: 8rem;">
+        Jail
+      </div>
+    </div>
+    <div class="bg-warning-subtle d-block ms-auto">
+      <div v-for="property in properties" :key="property" class="border border-dark" style="width: 8rem; height: 7rem;">
+        Cell#{{property}}
+      </div>
+    </div>
+    <div class="bg-warning-subtle d-block me-auto" style="margin-left: 63rem;">
+      <div v-for="property in properties" :key="property" class="border border-dark" style="width: 8rem; height: 7rem;">
+        Cell#{{property}}
+      </div>
+    </div>
+    <div class="bg-warning-subtle d-flex mx-auto">
+      <div class="border border-dark" style="width: 8rem; height: 8rem;">
+        Go to Jail
+      </div>
+      <div v-for="property in properties" :key="property" class="border border-dark" style="width: 7rem; height: 8rem;">
+        Cell#{{property}}
+      </div>
+      <div class="border border-dark" style="width: 8rem; height: 8rem;">
+        Free Parking
+      </div>
+    </div>
+  </div>
+</template>
